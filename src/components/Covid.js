6c075c4c875b0395aa77ec0a740ca590ace2c02c
Dheeraj <dheerajkumarblr@gmail.com>
2021-07-15 20:46:15 +0530

    made it live using timeintervale

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -4,39 +4,38 @@ import img1 from './images/1.jpg'
 import img2 from './images/2.jpg'
 import img3 from './images/3.jpg'
 import img4 from './images/4.jpg'
-
 const Covid = () => {
    const [data,setData]=useState([]);
     const getCovidData =async()=>{
-
+     
         try{
             const res= await fetch('https://api.covid19india.org/data.json')
             const actualData=await res.json();
-            console.log("hi");
             console.log(actualData);
             setData(actualData.statewise[0]);
-        }
+       }
         catch(err){
             console.log(err);
         }
     }
     useEffect(()=>{
-      getCovidData();
-        
-    },[]);
+      setInterval(()=>{
+        getCovidData();
+      },5000)
+    },[setData]);
     return (
         <>
         <center>
         <h3> 🔴 LIVE COVID-19 Tracker</h3>
         <h2>COVID-19 CORONAVIRUS TRACKER FOR INDIA</h2>
         </center>
-        <div class="cards-list">
+        <div className="cards-list">
       
-  <div class="card 1">
+  <div className="card 1">
     <center><h1>Country</h1></center>
     
-    <div class="card_image"> <img loading="lazy" src={img1} alt="img1" /> </div>
-    <div class="card_title title-white">
+    <div className="card_image"> <img loading="lazy" src={img1} alt="img1" /> </div>
+    <div className="card_title title-white">
       <p>INDIA</p>
     </div>
   </div>
@@ -88,6 +87,7 @@ const Covid = () => {
        <img loading="lazy" src={img4} alt="img" /> </div>
     <div class="card_title title-white">
       <p><b>LAST UPDATE</b></p>
+      <h3>Updating</h3>
     </div>
   </div>
   </div>
